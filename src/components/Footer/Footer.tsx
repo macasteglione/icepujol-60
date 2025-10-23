2e8e25fd49754bfa72cc08e89d31fe8bbec45899
macasteglione <matiascasteglione2002@gmail.com>
2025-10-22 23:12:40 -0300

    cambio de links sociales

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,54 +3,45 @@ import "./Footer.css";
 import { FaFacebook, FaInstagram, FaSpotify, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
-    const { t } = useTranslation();
-    const icon_size = 25;
+  const { t } = useTranslation();
+  const icon_size = 25;
 
-    return (
-        <footer>
-            <div className="footer-content">
-                <h3>{t("footerTitle")}</h3>
-                <p>{t("footerDescription")}</p>
-                <ul className="socials">
-                    <li>
-                        <a
-                            href="https://www.facebook.com/icepm.ok/"
-                            target="_blank"
-                        >
-                            <FaFacebook size={icon_size} />
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="https://www.instagram.com/icepm_ok/"
-                            target="_blank"
-                        >
-                            <FaInstagram size={icon_size} />
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="https://www.youtube.com/@icepm_ok"
-                            target="_blank"
-                        >
-                            <FaYoutube size={icon_size} />
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="https://open.spotify.com/show/0JW3aJOV5cHZtfEcaxyTFd"
-                            target="_blank"
-                        >
-                            <FaSpotify size={icon_size} />
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <div className="footer-bottom">
-                <p>{t("footerRights")}</p>
-            </div>
-        </footer>
-    );
+  return (
+    <footer>
+      <div className="footer-content">
+        <h3>{t("footerTitle")}</h3>
+        <p>{t("footerDescription")}</p>
+        <ul className="socials">
+          <li>
+            <a href="https://www.facebook.com/icepm.oficial/" target="_blank">
+              <FaFacebook size={icon_size} />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/icepm_oficial/" target="_blank">
+              <FaInstagram size={icon_size} />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/@icepm_oficial" target="_blank">
+              <FaYoutube size={icon_size} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://open.spotify.com/show/0JW3aJOV5cHZtfEcaxyTFd"
+              target="_blank"
+            >
+              <FaSpotify size={icon_size} />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="footer-bottom">
+        <p>{t("footerRights")}</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
