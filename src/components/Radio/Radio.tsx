944cf0e48345170d18d78311d517e6a79be43078
macasteglione <matiascasteglione2002@gmail.com>
2025-11-18 13:08:56 -0300

    modifique grilla de horarios

--- src/components/Radio/Radio.tsx
+++ src/components/Radio/Radio.tsx
@@ -11,6 +11,9 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Tabs,
+  Tab,
+  Box,
 } from "@mui/material";
 import logoImg from "../../assets/buenasNuevas.png";
 import { useTranslation } from "react-i18next";
@@ -37,53 +40,161 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 // @ts-ignore
 const Radio = ({ sourceRef }) => {
   const { t } = useTranslation();
-  const horarios = [
-    { hora: "00:00", programa: "A Través de la Biblia" },
-    { hora: "00:30", programa: "Crianza Reverente" },
-    { hora: "01:00", programa: "Correr para Ganar" },
-    { hora: "01:30", programa: "Momento Decisivo" },
-    { hora: "02:00", programa: "El Amor Que Vale" },
-    { hora: "02:30", programa: "Micro mensajes" },
-    { hora: "03:00", programa: "Entre Amigas" },
-    { hora: "03:30", programa: "Aviva Nuestros Corazones" },
-    { hora: "04:00", programa: "De la Biblia a la vida" },
-    { hora: "04:30", programa: "Renovando Tu Mente" },
-    { hora: "05:00", programa: "Predicación del Evangelio" },
-    { hora: "06:00", programa: "Pedrito el Pulpo" },
-    { hora: "06:30", programa: "Viajeros" },
-    { hora: "07:00", programa: "Cambios Profundos" },
-    { hora: "07:30", programa: "La Verdad en el Tubo de Ensayo" },
-    { hora: "08:00", programa: "De la Biblia a la vida" },
-    { hora: "08:30", programa: "Micro mensajes" },
-    { hora: "09:00", programa: "Tierra Firme" },
-    { hora: "09:30", programa: "Jungla Semántica" },
-    { hora: "10:00", programa: "Lectura Pública de la Biblia" },
-    { hora: "10:30", programa: "Gracia a Vosotros" },
-    { hora: "11:00", programa: "Preguntas y Respuestas" },
-    { hora: "11:30", programa: "Bonus Track" },
-    { hora: "12:00", programa: "A Través de la Biblia" },
-    { hora: "12:30", programa: "Crianza Reverente" },
-    { hora: "13:00", programa: "Correr para Ganar" },
-    { hora: "13:30", programa: "Momento Decisivo" },
-    { hora: "14:00", programa: "El Amor Que Vale" },
-    { hora: "14:30", programa: "Micro mensajes" },
-    { hora: "15:00", programa: "Entre Amigas" },
-    { hora: "15:30", programa: "Aviva Nuestros Corazones" },
-    { hora: "16:00", programa: "Lectura Pública de la Biblia" },
-    { hora: "16:30", programa: "Renovando Tu Mente" },
-    { hora: "17:00", programa: "Pedrito el Pulpo" },
-    { hora: "17:30", programa: "Viajeros" },
-    { hora: "18:00", programa: "Melodias con Historias" },
-    { hora: "18:30", programa: "Micro mensajes" },
-    { hora: "19:00", programa: "Cambios Profundos" },
-    { hora: "19:30", programa: "La Verdad en el Tubo de Ensayo" },
-    { hora: "20:00", programa: "Predicación del Evangelio" },
-    { hora: "21:00", programa: "Tierra Firme" },
-    { hora: "21:30", programa: "Jungla Semántica" },
-    { hora: "22:00", programa: "Lectura Pública de la Biblia" },
-    { hora: "22:30", programa: "Gracia a Vosotros" },
-    { hora: "23:00", programa: "Música Clásica" },
+  const [tabValue, setTabValue] = useState(0);
+
+  const horariosDia1 = [
+    { hora: "0:00", programa: "Gracia a Vosotros", maestro: "John F. MacArthur" },
+    { hora: "0:30", programa: "En Contacto Global", maestro: "Charles Stanley"},
+    { hora: "1:00", programa: "Predicación del Evangelio", maestro: "Hermanos de la Iglesia" },
+    { hora: "2:00", programa: "Sabiduría Internacional", maestro: "Stephen Davey" },
+    { hora: "2:30", programa: "Espacio Musical", maestro: "Varios Compositores" },
+    { hora: "3:00", programa: "Renovando Tu Mente", maestro: "Robert C. Sproul" },
+    { hora: "3:30", programa: "A Través de la Biblia - Alimento para el Alma", maestro: "Samuel Montoya, Esteban Larrosa" },
+    { hora: "4:00", programa: "Espacio Musical", maestro: "Varios Compositores" },
+    { hora: "4:30", programa: "Abre la Biblia", maestro: "Colin Smith" },
+    { hora: "5:00", programa: "Carlos Ruiz Devocional - Cambios Profundos", maestro: "Carlos Ruiz, Nicolás Tranchini" },
+    { hora: "5:30", programa: "Semillas al Aire", maestro: "Raúl Ferrero" },
+    { hora: "6:00", programa: "Visión para Vivir", maestro:"Chuck Swindoll" },
+    { hora: "6:30", programa: "Correr para Ganar", maestro: "Erwin Lutzer" },
+    { hora: "7:00", programa: "El Camino de la Vida", maestro: "Cornelio Rivera" },
+    { hora: "7:30", programa: "Espacio Musical", maestro: "Varios Compositores" },
+    { hora: "8:00", programa: "Momento Decisivo", maestro: "David Jeremiah" },
+    { hora: "8:30", programa: "El Amor Que Vale", maestro: "Adrián Rogers" },
+    { hora: "9:00", programa: "La Verdad en el Tubo de Ensayo", maestro: "Aldo Garrasino y Esteban Larrosa" },
+    { hora: "9:30", programa: "Aviva Nuestros Corazones", maestro: "Patricia de Saladin" },
+    { hora: "10:00", programa: "Lectura Pública de la Biblia - Estudio Bíblico", maestro: "Ministerio LPB, Llamada de Medianoche" },
+    { hora: "10:30", programa: "Espacio Musical", maestro: "Varios Compositores" },
+    { hora: "11:00", programa: "Temas Bíblicos", maestro: "Fundación Bíblica La Buena Semilla" },
+    { hora: "11:30", programa: "A Través de la Biblia", maestro: "Samuel Montoya" },
+    { hora: "12:00", programa: "Gracia a Vosotros", maestro: "John F. MacArthur" },
+    { hora: "12:30", programa: "En Contacto Global", maestro: "Charles Stanley" },
+    { hora: "13:00", programa: "Predicación del Evangelio", maestro: "Hermanos de la Iglesia" },
+    { hora: "14:00", programa: "Sabiduría Internacional", maestro: "Stephen Davey" },
+    { hora: "14:30", programa: "Abre la Biblia", maestro: "Colin Smith" },
+    { hora: "15:00", programa: "De la Biblia a la vida", maestro: "Patricia Namnún y Chárbela Salcedo" },
+    { hora: "15:30", programa: "Semillas al Aire", maestro: "Raúl Ferrero" },
+    { hora: "16:00", programa: "El Camino de la Vida", maestro: "Cornelio Rivera" },
+    { hora: "16:30", programa: "Visión para Vivir", maestro:"Chuck Swindoll" },
+    { hora: "17:00", programa: "Momento Decisivo", maestro: "David Jeremiah" },
+    { hora: "17:30", programa: "Correr para Ganar", maestro: "Erwin Lutzer" },
+    { hora: "18:00", programa: "Carlos Ruiz Devocional - Cambios Profundos", maestro: "Carlos Ruiz, Nicolás Tranchini" },
+    { hora: "18:30", programa: "Pedrito el Pulpo - Viajeros", maestro: "Radio Trans Mundial, Llamada de Medianoche" },
+    { hora: "19:00", programa: "La Verdad en el Tubo de Ensayo", maestro: "Aldo Garrasino y Esteban Larrosa" },
+    { hora: "19:30", programa: "Aviva Nuestros Corazones", maestro: "Patricia de Saladin" },
+    { hora: "20:00", programa: "Renovando Tu Mente", maestro: "Robert C. Sproul" },
+    { hora: "20:30", programa: "El Amor Que Vale", maestro: "Adrián Rogers" },
+    { hora: "21:00", programa: "Predicación del Evangelio", maestro: "Hermanos de la Iglesia" },
+    { hora: "22:00", programa: "Temas Bíblicos", maestro: "Fundación Bíblica La Buena Semilla" },
+    { hora: "22:30", programa: "Lectura Pública de la Biblia - Alimento para el Alma", maestro: "Ministerio LPB, Esteban Larrosa" },
+    { hora: "23:00", programa: "Música Clásica", maestro: "Varios Compositores" },
   ];
+
+  const horariosDia2 = [
+    { hora: "0:00", programa: "Gracia a Vosotros", maestro: "John F. MacArthur" },
+    { hora: "0:30", programa: "En Contacto Global", maestro: "Charles Stanley" },
+    { hora: "1:00", programa: "Predicación del Evangelio", maestro: "Hermanos de la Iglesia" },
+    { hora: "2:00", programa: "Sabiduría Internacional", maestro: "Stephen Davey" },
+    { hora: "2:30", programa: "Tierra Firme", maestro: "Esteban Larrosa y Salvador Dellutri" },
+    { hora: "3:00", programa: "Espacio Musical", maestro: "Varios Compositores" },
+    { hora: "3:30", programa: "A Través de la Biblia - Alimento para el Alma", maestro: "Samuel Montoya, Esteban Larrosa" },
+    { hora: "4:00", programa: "Espacio Musical", maestro: "Varios Compositores" },
+    { hora: "4:30", programa: "Fundamentos del Discipulado", maestro: "Esteban Larrosa" },
+    { hora: "5:00", programa: "Estudio Bíblico - Cambios Profundos", maestro: "Llamada de Medianoche, Nicolás Tranchini" },
+    { hora: "5:30", programa: "Semillas al Aire", maestro: "Raúl Ferrero" },
+    { hora: "6:00", programa: "Visión para Vivir", maestro:"Chuck Swindoll" },
+    { hora: "6:30", programa: "Correr para Ganar", maestro: "Erwin Lutzer" },
+    { hora: "7:00", programa: "El Camino de la Vida", maestro: "Cornelio Rivera" },
+    { hora: "7:30", programa: "Crianza Reverente", maestro: "Susi Bixby" },
+    { hora: "8:00", programa: "Momento Decisivo", maestro: "David Jeremiah" },
+    { hora: "8:30", programa: "El Amor Que Vale", maestro: "Adrián Rogers" },
+    { hora: "9:00", programa: "Jungla Semántica", maestro: "Esteban Larrosa y Héctor Leites" },
+    { hora: "9:30", programa: "Entre Amigas - Alimento para el Alma", maestro: "Llamada de Medianoche, Esteban Larrosa" },
+    { hora: "10:00", programa: "Lectura Pública de la Biblia", maestro: "Ministerio LPB" },
+    { hora: "10:30", programa: "El Contacto Cristiano - Preguntas y Respuestas", maestro: "Luis Ortega y Ricardo Robinson, Llamada de Medianoche" },
+    { hora: "11:00", programa: "Abre la Biblia", maestro: "Colin Smith" },
+    { hora: "11:30", programa: "A Través de la Biblia", maestro: "Samuel Montoya" },
+    { hora: "12:00", programa: "Gracia a Vosotros", maestro: "John F. MacArthur" },
+    { hora: "12:30", programa: "En Contacto Global", maestro: "Charles Stanley" },
+    { hora: "13:00", programa: "Predicación del Evangelio", maestro: "Hermanos de la Iglesia" },
+    { hora: "14:00", programa: "Sabiduría Internacional", maestro: "Stephen Davey" },
+    { hora: "14:30", programa: "Tierra Firme", maestro: "Esteban Larrosa y Salvador Dellutri" },
+    { hora: "15:00", programa: "Espacio Musical", maestro: "Varios Compositores" },
+    { hora: "15:30", programa: "Semillas al Aire", maestro: "Raúl Ferrero" },
+    { hora: "16:00", programa: "El Camino de la Vida", maestro: "Cornelio Rivera" },
+    { hora: "16:30", programa: "Visión para Vivir", maestro:"Chuck Swindoll" },
+    { hora: "17:00", programa: "Momento Decisivo", maestro: "David Jeremiah" },
+    { hora: "17:30", programa: "Correr para Ganar", maestro: "Erwin Lutzer" },
+    { hora: "18:00", programa: "Estudio Bíblico - Cambios Profundos", maestro: "Llamada de Medianoche, Nicolás Tranchini" },
+    { hora: "18:30", programa: "Melodías con Historia", maestro: "Equipo Buenas Nuevas" },
+    { hora: "19:00", programa: "Bonus Track - Alimento para el Alma", maestro: "Llamada de Medianoche, Esteban Larrosa" },
+    { hora: "19:30", programa: "Crianza Reverente", maestro: "Susi Bixby" },
+    { hora: "20:00", programa: "Jungla Semántica", maestro: "Esteban Larrosa y Héctor Leites" },
+    { hora: "20:30", programa: "El Amor Que Vale", maestro: "Adrián Rogers" },
+    { hora: "21:00", programa: "Predicación del Evangelio", maestro: "Hermanos de la Iglesia" },
+    { hora: "22:00", programa: "Lectura Pública de la Biblia", maestro: "Ministerio LPB" },
+    { hora: "22:30", programa: "Fundamentos del Discipulado", maestro: "Esteban Larrosa" },
+    { hora: "23:00", programa: "Música Clásica", maestro: "Varios Compositores" },
+  ];
+
+  const horariosFinDeSemana = [
+    { hora: "0:00", programa: "Música Clásica", maestro: "Varios Compositores" },
+    { hora: "1:00", programa: "Un Sermón para tu Semana", maestro: "Equipo de Coalición Por El Evangelio" },
+    { hora: "2:00", programa: "Predicación del Evangelio", maestro: "Hermanos de la Iglesia" },
+    { hora: "3:00", programa: "Proyecto Biblia Pódcast", maestro: "Tim Mackie y Jon Collins" },
+    { hora: "4:00", programa: "Gracia a Vosotros", maestro: "John F. MacArthur" },
+    { hora: "4:30", programa: "Espacio Musical", maestro: "Varios Compositores" },
+    { hora: "5:00", programa: "Carlos Ruiz Devocional - Cambios Profundos", maestro: "Carlos Ruiz, Nicolás Tranchini" },
+    { hora: "5:30", programa: "BITE Project", maestro: "Equipo de BITE Project" },
+    { hora: "6:00", programa: "Visión para Vivir", maestro:"Chuck Swindoll" },
+    { hora: "6:30", programa: "Semillas al Aire", maestro: "Raúl Ferrero" },
+    { hora: "7:00", programa: "Luis Palau Responde", maestro: "Luis Palau" },
+    { hora: "7:30", programa: "Pedrito el Pulpo - Viajeros", maestro: "Radio Trans Mundial, Llamada de Medianoche" },
+    { hora: "8:00", programa: "Espacio Musical", maestro: "Varios Compositores" },
+    { hora: "8:30", programa: "Fundamentos del Discipulado", maestro: "Esteban Larrosa" },
+    { hora: "9:00", programa: "Sabiduría Internacional", maestro: "Stephen Davey" },
+    { hora: "9:30", programa: "Coalición Podcast", maestro: "Equipo de Coalición Por El Evangelio" },
+    { hora: "10:00", programa: "El Contacto Cristiano - Estudio Bíblico", maestro: "Luis Ortega y Ricardo Robinson, Llamada de Medianoche" },
+    { hora: "10:30", programa: "Espacio Musical", maestro: "Varios Compositores" },
+    { hora: "11:00", programa: "Lectura Pública de la Biblia", maestro: "Ministerio LPB" },
+    { hora: "11:30", programa: "Espacio Musical", maestro: "Varios Compositores" },
+    { hora: "12:00", programa: "Un Sermón para tu Semana", maestro: "Equipo de Coalición Por El Evangelio" },
+    { hora: "13:00", programa: "Espacio Musical", maestro: "Varios Compositores" },
+    { hora: "13:30", programa: "Esperanza Segura", maestro: "J. Otis Yoder y J. Mark Horst" },
+    { hora: "14:00", programa: "Escuela Bíblica Trans Mundial", maestro: "Radio Trans Mundial" },
+    { hora: "14:30", programa: "BITE Project", maestro: "Equipo de BITE Project" },
+    { hora: "15:00", programa: "Proyecto Biblia Pódcast", maestro: "Tim Mackie y Jon Collins" },
+    { hora: "16:00", programa: "Abre la Biblia", maestro: "Colin Smith" },
+    { hora: "16:30", programa: "A Través de la Biblia - Alimento para el Alma", maestro: "Samuel Montoya, Esteban Larrosa" },
+    { hora: "17:00", programa: "Espacio Musical", maestro: "Varios Compositores" },
+    { hora: "17:30", programa: "Semillas al Aire", maestro: "Raúl Ferrero" },
+    { hora: "18:00", programa: "Carlos Ruiz Devocional - Cambios Profundos", maestro: "Carlos Ruiz, Nicolás Tranchini" },
+    { hora: "18:30", programa: "Pedrito el Pulpo - Viajeros", maestro: "Radio Trans Mundial, Llamada de Medianoche" },
+    { hora: "19:00", programa: "Luis Palau Responde", maestro: "Luis Palau" },
+    { hora: "19:30", programa: "Espacio Musical" , maestro: "Varios Compositores"},
+    { hora: "20:00", programa: "Predicación del Evangelio", maestro: "Hermanos de la Iglesia" },
+    { hora: "21:00", programa: "Gracia a Vosotros", maestro: "John F. MacArthur" },
+    { hora: "21:30", programa: "Espacio Musical", maestro: "Varios Compositores" },
+    { hora: "22:00", programa: "Lectura Pública de la Biblia", maestro: "Ministerio LPB" },
+    { hora: "22:30", programa: "Fundamentos del Discipulado", maestro: "Esteban Larrosa" },
+    { hora: "23:00", programa: "Música Clásica", maestro: "Varios Compositores" },
+  ];
+
+  const handleTabChange = (_event: any, newValue: number) => {
+    setTabValue(newValue);
+  };
+
+  const getCurrentSchedule = () => {
+    switch (tabValue) {
+      case 0:
+        return horariosDia1;
+      case 1:
+        return horariosDia2;
+      case 2:
+        return horariosFinDeSemana;
+      default:
+        return horariosDia1;
+    }
+  };
 
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentTime, setCurrentTime] = useState(0);
@@ -291,6 +402,28 @@ const Radio = ({ sourceRef }) => {
       </div>
 
       <div className="horarios-programas">
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs
+            value={tabValue}
+            onChange={handleTabChange}
+            variant="fullWidth"
+            sx={{
+              backgroundColor: "#d0dbee",
+              "& .MuiTab-root": {
+                color: "#18181f",
+                fontWeight: 600,
+              },
+              "& .Mui-selected": {
+                color: "#436ee6 !important",
+              },
+            }}
+          >
+            <Tab label="Lun-Mié-Vie" />
+            <Tab label="Mar-Jue" />
+            <Tab label="Sáb-Dom" />
+          </Tabs>
+        </Box>
+
         <TableContainer
           component={Paper}
           sx={{ maxHeight: 400, overflow: "auto" }}
@@ -304,14 +437,20 @@ const Radio = ({ sourceRef }) => {
                 <StyledTableCell align="center">
                   <h3>{t("radioPrograma")}</h3>
                 </StyledTableCell>
+                <StyledTableCell align="center">
+                  <h3>Maestro/s</h3>
+                </StyledTableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {horarios.map((row: any, index: any) => (
+              {getCurrentSchedule().map((row: any, index: any) => (
                 <StyledTableRow key={index}>
                   <StyledTableCell align="center">{row.hora}</StyledTableCell>
                   <StyledTableCell align="center">
                     {row.programa}
+                  </StyledTableCell>
+                  <StyledTableCell align="center">
+                    {row.maestro}
                   </StyledTableCell>
                 </StyledTableRow>
               ))}
