495fe0d982551c37accd45cee5e99b671e6b84be
macasteglione <matiascasteglione2002@gmail.com>
2025-12-06 20:10:43 -0300

    cambio de horarios

--- src/components/Horarios/Horarios.tsx
+++ src/components/Horarios/Horarios.tsx
@@ -1,177 +1,167 @@
 import {
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    Paper,
-    styled,
-    tableCellClasses,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  styled,
+  tableCellClasses,
 } from "@mui/material";
 import "./Horarios.css";
 import { useTranslation } from "react-i18next";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-        backgroundColor: theme.palette.common.black,
-        color: theme.palette.common.white,
-    },
-    [`&.${tableCellClasses.body}`]: {
-        fontSize: 14,
-    },
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    "&:nth-of-type(odd)": {
-        backgroundColor: theme.palette.action.hover,
-    },
-    "&:last-child td, &:last-child th": {
-        border: 0,
-    },
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
 }));
 
 const Horarios = () => {
-    const { t } = useTranslation();
-    const horarios = [
-        {
-            hora: "09:30",
-            domingo: t("horariosSantaCena"),
-            lunes: "-",
-            martes: "-",
-            miercoles: "-",
-            jueves: "-",
-            viernes: "-",
-            sabado: "-",
-        },
-        {
-            hora: "11:00",
-            domingo: t("horariosEscuelaDominical"),
-            lunes: "-",
-            martes: "-",
-            miercoles: "-",
-            jueves: "-",
-            viernes: "-",
-            sabado: "-",
-        },
-        {
-            hora: "14:30",
-            domingo: "-",
-            lunes: "-",
-            martes: "-",
-            miercoles: "-",
-            jueves: "-",
-            viernes: "-",
-            sabado: t("horariosReunionPreadolescentes"),
-        },
-        {
-            hora: "15:30",
-            domingo: "-",
-            lunes: t("horariosReunionFemenina"),
-            martes: "-",
-            miercoles: "-",
-            jueves: "-",
-            viernes: "-",
-            sabado: "-",
-        },
-        {
-            hora: "17:00",
-            domingo: "-",
-            lunes: "-",
-            martes: "-",
-            miercoles: "-",
-            jueves: "-",
-            viernes: "-",
-            sabado: t("horariosReunionAdolescentes"),
-        },
-        {
-            hora: "19:00",
-            domingo: t("horariosExplicacionEvangelio"),
-            lunes: "-",
-            martes: "-",
-            miercoles: "-",
-            jueves: "-",
-            viernes: "-",
-            sabado: "-",
-        },
-        {
-            hora: "20:00",
-            domingo: "-",
-            lunes: "-",
-            martes: t("horariosReunionOracion"),
-            miercoles: "-",
-            jueves: t("horariosEstudioBiblico"),
-            viernes: "-",
-            sabado: t("horariosReunionJovenes"),
-        }
-    ];
+  const { t } = useTranslation();
+  const horarios = [
+    {
+      hora: "09:30",
+      domingo: t("horariosSantaCena"),
+      lunes: "-",
+      martes: "-",
+      miercoles: "-",
+      jueves: "-",
+      viernes: "-",
+      sabado: "-",
+    },
+    {
+      hora: "11:00",
+      domingo: "-",
+      //domingo: t("horariosEscuelaDominical"),
+      lunes: "-",
+      martes: "-",
+      miercoles: "-",
+      jueves: "-",
+      viernes: "-",
+      sabado: "-",
+    },
+    {
+      hora: "14:30",
+      domingo: "-",
+      lunes: "-",
+      martes: "-",
+      miercoles: "-",
+      jueves: "-",
+      viernes: "-",
+      sabado: "-",
+      //sabado: t("horariosReunionPreadolescentes"),
+    },
+    {
+      hora: "15:30",
+      domingo: "-",
+      lunes: t("horariosReunionFemenina"),
+      martes: "-",
+      miercoles: "-",
+      jueves: "-",
+      viernes: "-",
+      sabado: "-",
+    },
+    {
+      hora: "17:00",
+      domingo: "-",
+      lunes: "-",
+      martes: "-",
+      miercoles: "-",
+      jueves: "-",
+      viernes: "-",
+      sabado: "-",
+      //sabado: t("horariosReunionAdolescentes"),
+    },
+    {
+      hora: "19:00",
+      domingo: t("horariosExplicacionEvangelio"),
+      lunes: "-",
+      martes: "-",
+      miercoles: "-",
+      jueves: "-",
+      viernes: "-",
+      sabado: "-",
+    },
+    {
+      hora: "20:00",
+      domingo: "-",
+      lunes: "-",
+      martes: t("horariosReunionOracion"),
+      miercoles: "-",
+      jueves: t("horariosEstudioBiblico"),
+      viernes: t("horariosReunionJovenes"),
+      sabado: "-",
+      //sabado: t("horariosReunionJovenes"),
+    },
+  ];
 
-    return (
-        <div className="horarios">
-            <TableContainer component={Paper}>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <StyledTableCell align="center">
-                                <h3>{t("horariosHora")}</h3>
-                            </StyledTableCell>
-                            <StyledTableCell align="center">
-                                <h3>{t("horariosDomingo")}</h3>
-                            </StyledTableCell>
-                            <StyledTableCell align="center">
-                                <h3>{t("horariosLunes")}</h3>
-                            </StyledTableCell>
-                            <StyledTableCell align="center">
-                                <h3>{t("horariosMartes")}</h3>
-                            </StyledTableCell>
-                            <StyledTableCell align="center">
-                                <h3>{t("horariosMiercoles")}</h3>
-                            </StyledTableCell>
-                            <StyledTableCell align="center">
-                                <h3>{t("horariosJueves")}</h3>
-                            </StyledTableCell>
-                            <StyledTableCell align="center">
-                                <h3>{t("horariosViernes")}</h3>
-                            </StyledTableCell>
-                            <StyledTableCell align="center">
-                                <h3>{t("horariosSabado")}</h3>
-                            </StyledTableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {horarios.map((row: any, index: any) => (
-                            <StyledTableRow key={index}>
-                                <StyledTableCell align="center">
-                                    {row.hora}
-                                </StyledTableCell>
-                                <StyledTableCell align="center">
-                                    {row.domingo}
-                                </StyledTableCell>
-                                <StyledTableCell align="center">
-                                    {row.lunes}
-                                </StyledTableCell>
-                                <StyledTableCell align="center">
-                                    {row.martes}
-                                </StyledTableCell>
-                                <StyledTableCell align="center">
-                                    {row.miercoles}
-                                </StyledTableCell>
-                                <StyledTableCell align="center">
-                                    {row.jueves}
-                                </StyledTableCell>
-                                <StyledTableCell align="center">
-                                    {row.viernes}
-                                </StyledTableCell>
-                                <StyledTableCell align="center">
-                                    {row.sabado}
-                                </StyledTableCell>
-                            </StyledTableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </div>
-    );
+  return (
+    <div className="horarios">
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="center">
+                <h3>{t("horariosHora")}</h3>
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                <h3>{t("horariosDomingo")}</h3>
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                <h3>{t("horariosLunes")}</h3>
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                <h3>{t("horariosMartes")}</h3>
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                <h3>{t("horariosMiercoles")}</h3>
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                <h3>{t("horariosJueves")}</h3>
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                <h3>{t("horariosViernes")}</h3>
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                <h3>{t("horariosSabado")}</h3>
+              </StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {horarios.map((row: any, index: any) => (
+              <StyledTableRow key={index}>
+                <StyledTableCell align="center">{row.hora}</StyledTableCell>
+                <StyledTableCell align="center">{row.domingo}</StyledTableCell>
+                <StyledTableCell align="center">{row.lunes}</StyledTableCell>
+                <StyledTableCell align="center">{row.martes}</StyledTableCell>
+                <StyledTableCell align="center">
+                  {row.miercoles}
+                </StyledTableCell>
+                <StyledTableCell align="center">{row.jueves}</StyledTableCell>
+                <StyledTableCell align="center">{row.viernes}</StyledTableCell>
+                <StyledTableCell align="center">{row.sabado}</StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 };
 
 export default Horarios;
